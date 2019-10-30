850d67e8b3f7cdf37bd11bddac942608fc6cbecc
Kaylee Laurine <klaurine@at-n3.21-school.ru>
2019-10-30 19:39:36 +0300

    301019

--- 03/3-0-print_hex/print_hex.c
+++ 03/3-0-print_hex/print_hex.c
@@ -1,28 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tab_mult.c                                         :+:      :+:    :+:   */
+/*   print_hex.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: klaurine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/26 13:45:34 by klaurine          #+#    #+#             */
-/*   Updated: 2019/10/29 17:53:12 by klaurine         ###   ########.fr       */
+/*   Created: 2019/10/28 16:03:29 by klaurine          #+#    #+#             */
+/*   Updated: 2019/10/30 19:38:51 by klaurine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	mini_putnbr(int i)
-{
-	if (i > 9)
-		mini_putnbr(i / 10);
-	ft_putchar(i % 10 + '0');
-}
 
 int		mini_atoi(char *s)
 {
@@ -37,27 +25,21 @@ int		mini_atoi(char *s)
 	return (number);
 }
 
+void	change_number(int number)
+{
+	if (number >= 16)
+		change_number (number / 16);
+	
+
+
 int		main(int ac, char **av)
 {
-	int i;
 	int num;
 
-	i = 1;
-	if (ac != 2)
-		write(1, "\n", 1);
-	else
+	if (2 == ac)
 	{
 		num = mini_atoi(av[1]);
-		while (i <= 9)
-		{
-			mini_putnbr(i);
-			write(1, " x ", 3);
-			mini_putnbr(num);
-			write(1, " = ", 3);
-			mini_putnbr(i * num);
-			write(1, "\n", 1);
-			i++;
-		}
+		change_number(num);
 	}
+	write(1, "\n", );
 	return (0);
-}
