05eaad0b63a04542aeb14f7385be5e7479c2315a
Kaylee Laurine <klaurine@mi-w3.21-school.ru>
2020-01-17 21:35:06 +0300

    170120

--- 04/ft_itoa_base/ft_itoa_base.c
+++ 04/ft_itoa_base/ft_itoa_base.c
@@ -20,24 +20,34 @@ char    *ft_itoa_base(int value, int base)
     long number;
 
     sign = (10 == base && value < 0) ? 1 : 0;
-    if (value < 0)
-        number = -(long)value;
+    number = (value < 0) ? -(long)value : value;
     i = (1 == sign) ? 2 : 1;
     while (number > 9)
     {
         i++;
         number /= 10;
     }
-    if (!(s = (char *)malloc(sizeof(char) * (i + 1))))
+    if (!(s = (char *) malloc(sizeof(char) * (i + 1))))
         return (NULL);
-    
-
-
-
-
-
-
-
-
-
+    s[i] = '\0';
+    number = (value < 0) ? -(long)value : value;
+    while (--i >= 0)
+    {
+        s[i] = (number % base < 10) ? number % base + '0' : number % base + 'A' - 10;
+        number /= base;
+    }
+    if (1 == sign)
+        s[++i] = '-';
+    return (s);
 }
+
+/*
+#include <stdio.h>
+
+int     main(int ac, char **av)
+{
+    if (3 == ac)
+        printf("%s", ft_itoa_base(atoi(av[1]), atoi(av[2])));
+    return (0);
+}
+*/
