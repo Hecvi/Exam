c6789038d97746e71e13c9a9abae636764f96aac
Дмитрий Бондаренко <hecvi@MacBook-Pro-Dmitrij.local>
2020-01-19 23:05:18 +0300

    190120

--- 05/biggest_pal/biggest_pal.c
+++ 05/biggest_pal/biggest_pal.c
@@ -3,114 +3,134 @@
 /*                                                        :::      ::::::::   */
 /*   biggest_pal.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: klaurine <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/14 13:30:10 by klaurine          #+#    #+#             */
-/*   Updated: 2020/01/14 13:30:12 by klaurine         ###   ########.fr       */
+/*   Created: 2020/01/18 12:02:26 by exam              #+#    #+#             */
+/*   Updated: 2020/01/18 12:02:32 by exam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int     check_pal(char *s, int i, int nechet, int counter)
+int     check_nechet(char *s, int i)
 {
     int j;
+    int counter;
 
     j = 1;
-    if (1 == nechet)
-        while (s[i + j] == s[i - j] && (i - j) >= 0 && (i + j))
-        {
-            counter++;
-            j++;
-        }
-    else if (0 == nechet)
-        while (s[i + j + 1] == s[i - j] && (i - j) >= 0 && (i + j + 1))
-        {
-            counter++;
-            j++;
-        }
+    counter = 0;
+    while (s[i + j] == s[i - j] && (i - j) >= 0 && s[i + j])
+    {
+        counter++;
+        j++;
+    }
     return (counter);
 }
 
-void    write_pal(char *s, int max, int i, int nechet)
+int     check_chet(char *s, int i)
 {
-    int tmp;
+    int j;
+    int counter;
 
-    tmp = max;
-    if (1 == nechet)
+    j = 1;
+    counter = 1;
+    while (s[i - j] == s[i + j + 1] && (i - j) >= 0 && s[i + j] && s[i + j + 1])
     {
-        while (max > 0)
-        {
-            write(1, &s[i - max], 1);
-            max--;
-        }
-        while (max <= tmp)
-        {
-            write(1, &s[i + max], 1);
-            max++;
-        }
+        counter++;
+        j++;
     }
-    else
+    return (counter);
+}
+
+void    print_nechet(char *s, int j, int i)
+{
+    int max;
+
+    max = j;
+    while (j)
     {
-        max--;
-        while (max >= 0)
-        {
-            write(1, &s[i - max], 1);
-            max--;
-        }
-        while (++max < tmp)
-            write(1, &s[i + max + 1], 1);
+        write(1, &s[i - j], 1);
+        j--;
+    }
+    while (j <= max)
+    {
+        write(1, &s[i - j], 1);
+        j++;
+    }
+}
+
+void    print_chet(char *s, int j, int i)
+{
+    int max;
+
+    j--;
+    max = j;
+    while (j >= 0)
+    {
+        write(1, &s[i - j], 1);
+        j--;
+    }
+    j++;
+    while (j <= max)
+    {
+        write(1, &s[i - j], 1);
+        j++;
     }
 }
 
 int     main(int ac, char **av)
 {
     int i;
-    int chet;
-    int start;
-    int nechet;
-    int max_chet;
-    int max_nechet;
+    int max_n;
+    int max_c;
+    int start_n;
+    int start_c;
+    int counter_n;
+    int counter_c;
 
-    if (2 == ac && av[1][0])
+
+    if (2 == ac)
     {
-        i = 1;
-        start = 0;
-        max_chet = 0;
-        max_nechet = 0;
-        if (av[1][0] && '\0' == av[1][1])
+        if (av[1][0] && !av[1][1])
             write(1, av[1], 1);
-        else if (av[1][0] && av[1][1] && '\0' == av[1][2] && av[1][0] == av[1][1])
+        else if (av[1][0] && av[1][1] && !av[1][2] && av[1][0] == av[1][1])
             write(1, av[1], 2);
         else
+        {
+            i = 0;
+            max_n = 0;
+            max_c = 0;
+            start_n = 0;
+            start_c = 0;
+            counter_n = 0;
+            counter_c = 0;
             while (av[1][i])
             {
-                chet = 0;
-                nechet = 0;
-                if (av[1][i - 1] == av[1][i + 1] && (i - 1) >= 0 && (i + 1))
+                if (av[1][i + 1] == av[1][i - 1] && (i - 1) >= 0 && av[1][i + 1])
                 {
-                    nechet = check_pal(av[1], i, 1, 0);
-                    if (nechet > max_nechet)
+                    counter_n = check_nechet(av[1], i);
+                    if (counter_n >= max_n)
                     {
-                        max_nechet = nechet;
-                        start = i;
+                        max_n = counter_n;
+                        start_n = i;
                     }
                 }
-                if (av[1][i] == av[1][i + 1] && av[1][i - 1] == av[1][i + 2] && (i - 1) >= 0 && (i + 2))
+                else if (av[1][i] == av[1][i + 1] && av[1][i - 1] == av[1][i + 2] && (i - 1) >= 0 && av[1][i + 1] && av[1][i + 2])
                 {
-                    chet = check_pal(av[1], i, 0, 1);
-                    if (chet > max_chet)
+                    counter_c = check_chet(av[1], i);
+                    if (counter_c >= max_c)
                     {
-                        max_chet = chet;
-                        start = i;
+                        max_c = counter_c;
+                        start_c = i;
                     }
                 }
                 i++;
             }
-            if (max_nechet > max_chet)
-                write_pal(av[1], max_nechet, start, 1);
-            else if (max_nechet < max_chet)
-            write_pal(av[1], max_chet, start, 0);
+            if (max_n >= max_c && max_n)
+                print_nechet(av[1], max_n, start_n);
+            else if (max_c > max_n && max_c)
+                print_chet(av[1], max_c, start_c);
+        }
     }
     write(1, "\n", 1);
     return (0);
